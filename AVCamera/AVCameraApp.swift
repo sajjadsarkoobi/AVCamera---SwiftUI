154c760e4986fb0c96491b1a28f43f5e8a96da94
Sajjad Sarkoobi <sajjadsarkoobi@gmail.com>
2022-12-22 15:20:55 +0300

    Initial Commit

---
+++ AVCamera/AVCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVCameraApp.swift
+//  AVCamera
+//
+//  Created by Sajjad Sarkoobi on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct AVCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
